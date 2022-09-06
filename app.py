db527e545400f47ca93239692baba25e4b366e12
Hillman <no.word.about.me@hotmail.com>
2022-09-06 00:44:10 -0400

    combine show and add function

--- app.py
+++ app.py
@@ -31,52 +31,45 @@ VerifEntry5 = Entry(root, width = 10)
 #Tkinter functions
 def addDead():
     DEAD_LETTERS.append(str(D1Entry.get()))
-def showDead():
     DeadLabel = Label(root, text = str(DEAD_LETTERS))
     DeadLabel.grid(row = 1)
-    print(DEAD_LETTERS)
+
 def addDic():
     if str(DicEntry.get()) in CORRECT_LETTERS_WRONG_POSITIONS:
         CORRECT_LETTERS_WRONG_POSITIONS[str(DicEntry.get())].append(int(ValEntry.get()))
     else:
         CORRECT_LETTERS_WRONG_POSITIONS[str(DicEntry.get())] = [int(ValEntry.get())]
-def showDic():
     DicLabel = Label(root, text = str(CORRECT_LETTERS_WRONG_POSITIONS))
     DicLabel.grid(row = 3)
+
 def addVeri():
     VERIFIED_LETTERS[0] = str(VerifEntry1.get())
     VERIFIED_LETTERS[1] = str(VerifEntry2.get())
     VERIFIED_LETTERS[2] = str(VerifEntry3.get())
     VERIFIED_LETTERS[3] = str(VerifEntry4.get())
     VERIFIED_LETTERS[4] = str(VerifEntry5.get())
-def showVeri():
     VeriLabel = Label(root, text = str(VERIFIED_LETTERS))
-    VeriLabel.grid()
+    VeriLabel.grid(row = 10)
+
+    
 
 #Tkinter button setup
 EntryAdd = Button(root, text = 'Add Dead Letters', command = addDead)
-EntryShow = Button (root, text = 'Show Dead Letters', command = showDead)
 DicAdd = Button(root, text = 'Add flawed Dictionary', command = addDic)
-DicShow = Button(root, text = 'Show flawed Dictionary', command = showDic)
 VeriAdd = Button(root, text = 'Add Verified Letters', command = addVeri)
-VeriShow = Button(root, text = 'Show Verified Letters', command = showVeri)
-# TODO: Add a result generator button at the end
 
 #Layout of Buttons and Entrys
 D1Entry.grid(row = 0, column = 0, columnspan = 5)
 EntryAdd.grid(row = 0, column = 5, columnspan = 2)
-EntryShow.grid(row = 0, column = 7, columnspan = 2)
 DicEntry.grid(row = 2, column = 0, columnspan = 2)
 ValEntry.grid(row = 2, column = 2, columnspan = 2)
 DicAdd.grid(row = 2, column = 4, columnspan = 2)
-DicShow.grid(row = 2, column = 6, columnspan = 2)
 VerifEntry1.grid(row = 4, column = 0, columnspan = 2)
 VerifEntry2.grid(row = 4, column = 2, columnspan = 2)
 VerifEntry3.grid(row = 4, column = 4, columnspan = 2)
 VerifEntry4.grid(row = 4, column = 6, columnspan = 2)
 VerifEntry5.grid(row = 4, column = 8, columnspan = 2)
 VeriAdd.grid(row = 5, column = 0, columnspan = 3)
-VeriShow.grid(row = 5, column = 3, columnspan = 3)
 
 
 """Do not edit code below this line!"""
@@ -235,12 +228,8 @@ def get_best_guess(answer_list):
 def _read_file(filename):
     with open(filename, 'r') as data:
         word_list = json.load(data)
-
     return word_list
 
-
-# if __name__ == '__main__':
-#     main()
 
 def clearAnswer():
     answerList.clear()
